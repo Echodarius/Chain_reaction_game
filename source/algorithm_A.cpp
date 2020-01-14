bdcdf548bb9b6f2bc8212b72e34ee8d4687f2c8f
張兆尹 <b03b01049@g.ntu.edu.tw>
2020-01-14 20:41:42 +0800

    [Modify] modify "evaluate_board" function , and add "Minimax" function

--- source/algorithm_A.cpp
+++ source/algorithm_A.cpp
@@ -26,217 +26,160 @@ using namespace std;
  * 4. The function that print out the current board statement
 *************************************************************************/
 
-int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
-int evaluate_board(Board board, char player_color, char my_color);
-int** get_legal_moves(Board board, char player_color, int &move_number);
-void Minimax(Board board, char player_color, char my_color,
-    int index[], int depth, int alpha, int beta, int score);
+int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+int corner[4][2] = {{0, 0}, {0, COL}, {ROW, 0}, {ROW, COL}};
+Player opponent(Board board, Player me);
+int evaluate_board(Board board, char my_color, char enemy_color);
+int **get_legal_moves(Board board, char player_color, int &move_number);
+int Minimax(Board board, bool maximizer, Player me, Player enemy,
+            int index[], int depth, int alpha, int beta);
 
 void algorithm_A(Board board, Player player, int index[])
 {
 
-    // cout << board.get_capacity(0, 0) << endl;
-    // cout << board.get_orbs_num(0, 0) << endl;
-    // cout << board.get_cell_color(0, 0) << endl;
-    // board.print_current_board(0, 0, 0);
+    Player enemy = opponent(board, player);
+    Minimax(board, true, player, enemy, index, 0, -10000, 10000);
 
-    //////////// Random Algorithm ////////////
-    // Here is the random algorithm for your reference, you can delete or comment it.
-
-    int row, col;
-    int mycolor = player.get_color();
-    cin >> index[0];
-    cin >> index[1];
+    cout << "I put at (" << index[0] << "," << index[1] << ")" << endl;
     cin.get();
 }
 
-int evaluate_board(Board board, char player_color, char my_color)
+Player opponent(Board board, Player me)
+{
+    char op_color = 'v';
+    bool found = false;
+    for (int i = 0; i < ROW; ++i)
+    {
+        for (int j = 0; j < COL; ++j)
+        {
+            if (board.get_cell_color(i, j) != 'w' && board.get_cell_color(i, j) != me.get_color())
+            {
+                op_color = board.get_cell_color(i, j);
+                found = true;
+                break;
+            }
+        }
+        if (found == true)
+        {
+            break;
+        }
+    }
+    Player op(op_color);
+    return op;
+}
+
+int evaluate_board(Board board, char my_color, char enemy_color)
 {
     int sc = 0;
-    int player_cell = 0, enemy_cell = 0;
+    int my_cell = 0, enemy_cell = 0;
     int nb_row = 0, nb_col = 0;
     int player_critical_cell = 0;
     int player_n_cont_cri_cell = 0; //my not contiguous critial cell
 
-    if (player_color == my_color)
+    for (int i = 0; i < ROW; ++i)
     {
-        for (int i = 0; i < ROW; ++i)
+        for (int j = 0; j < COL; ++j)
         {
-            for (int j = 0; j < COL; ++j)
+            if (board.get_cell_color(i, j) == my_color)
             {
-                if (board.get_cell_color(i, j) == my_color)
+                my_cell += 1;
+                bool vulnerable = false;
+                //neighbor cell
+                for (int k = 0; k < 4; ++k)
                 {
-                    player_cell += 1;
-                    bool vulnerable = false;
-                    //neighbor cell
+                    nb_row = i + dirs[k][0];
+                    nb_col = j + dirs[k][1];
+                    if (0 <= nb_row && nb_row < ROW &&
+                        0 <= nb_col && nb_col < COL)
+                    {
+                        if (board.get_cell_color(nb_row, nb_col) == enemy_color &&
+                            board.get_capacity(nb_row, nb_col) == board.get_orbs_num(nb_row, nb_col) + 1)
+                        {
+                            sc -= 5 - board.get_orbs_num(nb_row, nb_col);
+                            vulnerable = true;
+                        }
+                    }
+                }
+
+                if (!vulnerable)
+                {
+                    if (board.get_capacity(i, j) == 2)
+                        sc += 3;
+                    else if (board.get_capacity(i, j) == 3)
+                        sc += 2;
+                    if (board.get_capacity(i, j) == board.get_orbs_num(i, j) + 1)
+                        sc += 2;
+                }
+                if (board.get_capacity(i, j) == board.get_orbs_num(i, j) + 1)
+                {
+                    ++player_critical_cell;
+                    bool contiguous = false;
                     for (int k = 0; k < 4; ++k)
                     {
                         nb_row = i + dirs[k][0];
                         nb_col = j + dirs[k][1];
-                        if (0 <= nb_row && nb_row < ROW &&
-                            0 <= nb_col && nb_col < COL)
+                        if (0 <= nb_row && nb_row < ROW && 0 <= nb_col && nb_col < COL &&
+                            board.get_cell_color(nb_row, nb_col) == my_color &&
+                            board.get_capacity(nb_row, nb_col) == board.get_orbs_num(nb_row, nb_col) + 1)
                         {
-                            if (board.get_cell_color(nb_row, nb_col) != my_color &&
-                                board.get_capacity(nb_row, nb_col) == board.get_orbs_num(nb_row, nb_col) + 1)
-                            {
-                                sc -= 5 - board.get_orbs_num(nb_row, nb_col);
-                                vulnerable = true;
-                            }
+                            contiguous = true;
+                            break;
                         }
                     }
-
-                    if (!vulnerable)
+                    if (contiguous == false)
                     {
-                        if (board.get_capacity(i, j) == 2)
-                            sc += 3;
-                        else if (board.get_capacity(i, j) == 3)
-                            sc += 2;
-                        if (board.get_capacity(i, j) == board.get_orbs_num(i, j) + 1)
-                            sc += 2;
+                        ++player_n_cont_cri_cell;
                     }
-                    if (board.get_capacity(i, j) == board.get_orbs_num(i, j) + 1)
-                    {
-                        ++player_critical_cell;
-                        bool contiguous = false;
-                        for (int k = 0; k < 4; ++k)
-                        {
-                            nb_row = i + dirs[k][0];
-                            nb_col = j + dirs[k][1];
-                            if (0 <= nb_row && nb_row < ROW && 0 <= nb_col && nb_col < COL &&
-                                board.get_cell_color(nb_row, nb_col) == my_color &&
-                                board.get_capacity(nb_row, nb_col) == board.get_orbs_num(nb_row, nb_col) + 1)
-                            {
-                                contiguous = true;
-                                break;
-                            }
-                        }
-                        if (contiguous == false)
-                        {
-                            ++player_n_cont_cri_cell;
-                        }
-                    }
-                }
-                else
-                {
-                    enemy_cell += 1;
                 }
             }
-        }
-        sc += player_cell;
-        if (enemy_cell == 0 && player_cell > 1)
-            return 10000;
-        if (player_cell == 0 && enemy_cell > 1)
-            return -10000;
-        sc += 2 * (player_critical_cell - player_n_cont_cri_cell);
-        return sc;
-    }
-    else
-    {
-        for (int i = 0; i < ROW; ++i)
-        {
-            for (int j = 0; j < COL; ++j)
+            else
             {
-                if (board.get_cell_color(i, j) == 'V')
-                {
-                    player_cell += 1;
-                    bool vulnerable = false;
-                    //neighbor cell
-                    for (int k = 0; k < 4; ++k)
-                    {
-                        nb_row = i + dirs[k][0];
-                        nb_col = j + dirs[k][1];
-                        if (0 <= nb_row && nb_row < ROW &&
-                            0 <= nb_col && nb_col < COL)
-                        {
-                            if (board.get_cell_color(nb_row, nb_col) != my_color &&
-                                board.get_capacity(nb_row, nb_col) == board.get_orbs_num(nb_row, nb_col) + 1)
-                            {
-                                sc += 5 - board.get_orbs_num(nb_row, nb_col);
-                                vulnerable = true;
-                            }
-                        }
-                    }
-
-                    if (!vulnerable)
-                    {
-                        if (board.get_capacity(i, j) == 2)
-                            sc -= 3;
-                        else if (board.get_capacity(i, j) == 3)
-                            sc -= 2;
-                        if (board.get_capacity(i, j) == board.get_orbs_num(i, j) + 1)
-                            sc -= 2;
-                    }
-                    if (board.get_capacity(i, j) == board.get_orbs_num(i, j) + 1)
-                    {
-                        ++player_critical_cell;
-                        bool contiguous = false;
-                        for (int k = 0; k < 4; ++k)
-                        {
-                            nb_row = i + dirs[k][0];
-                            nb_col = j + dirs[k][1];
-                            if (0 <= nb_row && nb_row < ROW && 0 <= nb_col && nb_col < COL &&
-                                board.get_cell_color(nb_row, nb_col) == my_color &&
-                                board.get_capacity(nb_row, nb_col) == board.get_orbs_num(nb_row, nb_col) + 1)
-                            {
-                                contiguous = true;
-                                break;
-                            }
-                        }
-                        if (contiguous == false)
-                        {
-                            ++player_n_cont_cri_cell;
-                        }
-                    }
-                }
-                else
-                {
-                    enemy_cell += 1;
-                }
+                enemy_cell += 1;
             }
         }
-        sc += player_cell;
-        if (enemy_cell == 0 && player_cell > 1)
-            return -10000;
-        if (player_cell == 0 && enemy_cell > 1)
-            return 10000;
-        sc -= 2 * (player_critical_cell - player_n_cont_cri_cell);
-        return sc;
     }
+    sc += my_cell;
+    if (enemy_cell == 0 && my_cell > 1)
+        return 10000;
+    if (my_cell == 0 && enemy_cell > 1)
+        return -10000;
+    sc += 2 * (player_critical_cell - player_n_cont_cri_cell);
+    return sc;
 }
 
-int** get_legal_moves(Board board, Player player, int &move_number)
+int **get_legal_moves(Board board, char player_color, int &move_number)
 {
-    for(int i = 0; i < ROW; ++i)
+    for (int i = 0; i < ROW; ++i)
     {
-        for(int j = 0; j < COL; ++j)
+        for (int j = 0; j < COL; ++j)
         {
-            if(board.get_cell_color(i,j) == player.get_color() ||
-                board.get_cell_color(i,j) == 'w')
+            if (board.get_cell_color(i, j) == player_color ||
+                board.get_cell_color(i, j) == 'w')
             {
                 ++move_number;
             }
         }
     }
 
-    if(move_number == 0)
+    if (move_number == 0)
         return nullptr;
-    
-    int** legal = new int*[move_number];
-    for(int i = 0; i < move_number; ++i)
+
+    int **legal = new int *[move_number];
+    for (int i = 0; i < move_number; ++i)
     {
         legal[i] = new int[2];
     }
     int k = 0;
-    for(int i = 0; i < ROW; ++i)
+    for (int i = 0; i < ROW; ++i)
     {
-        for(int j = 0; j < COL; ++j)
+        for (int j = 0; j < COL; ++j)
         {
-            if(board.get_cell_color(i,j) == player.get_color ||
-                board.get_cell_color(i,j) == 'w')
+            if (board.get_cell_color(i, j) == player_color ||
+                board.get_cell_color(i, j) == 'w')
             {
                 legal[k][0] = i;
                 legal[k][1] = j;
-                if(k == move_number)
+                if (k == move_number)
                 {
                     return legal;
                 }
@@ -247,38 +190,92 @@ int** get_legal_moves(Board board, Player player, int &move_number)
     return legal;
 }
 
-void Minimax(Board board, Player player, Player me, 
-    int index[], int depth, int alpha, int beta, int score)
+Board psuedo_place_orb(Board board, Player &player, int i, int j)
+{
+    board.place_orb(i, j, &player);
+    return board;
+}
+
+int Minimax(Board board, bool maximizer, Player me, Player enemy,
+            int index[], int depth, int alpha, int beta)
 {
 
-    int best_score = (player.get_color == me.get_color()) ? 10000 : -10000;
+    int best_score = evaluate_board(board, me.get_color(), enemy.get_color());
 
-    if(depth == 3 || score == 10000 || score == -10000)
+    if(depth == 0 && (best_score == -10000 || best_score == 0))
     {
-        best_score = score;
-        return;
+        //get corner
+        for(int i = 0; i < 4; ++i)
+        {
+            if(board.get_cell_color(corner[i][0],corner[i][1]) == 'w' )
+            {
+                index[0] = corner[i][0];
+                index[1] = corner[i][1];
+                return 0;
+            }
+        }
     }
 
-    int legal_move_number = 0;  
-    int** legal = get_legal_moves(board, player, legal_move_number);
-
-    int cur_pos[2];
-    for(int i = 0; i < legal_move_number; ++i)
+    if (depth == 1 || best_score == 10000 || best_score == -10000)
     {
-        cur_pos[0] = legal[i][0];
-        cur_pos[1] = legal[i][1];
-        board.place_orb(cur_pos[0], cur_pos[1], &player);
-        
-        //Maximizer
-        if(player.get_color() == me.get_color())
-        {
-
-        }
-        //minimizer
-        else
-        {
-
-        }
-
+        return best_score;
     }
+
+    int legal_move_number = 0;
+    int **legal;
+    Board cur_board;
+
+    //Maximizer turn = my turn
+    if (maximizer)
+    {
+        //cout << "maximizer turn" << endl;
+        legal = get_legal_moves(board, me.get_color(), legal_move_number);
+        //cout << "best_score = " << best_score << endl;
+        for (int i = 0; i < legal_move_number; ++i)
+        {
+            cur_board = psuedo_place_orb(board, me, legal[i][0], legal[i][1]);
+            int score = Minimax(cur_board, false, me, enemy, index, depth + 1, alpha, beta);
+            cout << "x= " << legal[i][0] << ", y=" << legal[i][1] << ", score =" << score << endl;
+            if (best_score < score)
+            {
+                //cout << "b<s, "
+                //     << "x= " << legal[i][0] << ", y=" << legal[i][1] << ", score =" << score << endl;
+                best_score = score;
+                index[0] = legal[i][0];
+                index[1] = legal[i][1];
+                alpha = max(score, alpha);
+                if (beta <= alpha)
+                {
+                    break;
+                }
+            }
+        }
+    } //minimizer, enemy turn
+    else
+    {
+        //cout << "minimizer" << endl;
+        //cout << "best_score = " << best_score << endl;
+        legal = get_legal_moves(board, enemy.get_color(), legal_move_number);
+        for (int i = 0; i < legal_move_number; ++i)
+        {
+            cur_board = psuedo_place_orb(board, enemy, legal[i][0], legal[i][1]);
+            int score = Minimax(cur_board, true, me, enemy, index, depth + 1, alpha, beta);
+            //cout << "x= " << legal[i][0] << ", y=" << legal[i][1] << ", score =" << score << endl;
+            if (best_score >= score)
+            {
+                //cout << "b>s "
+                //     << "x= " << legal[i][0] << ", y=" << legal[i][1] << ", score =" << score << endl;
+                best_score = score;
+                index[0] = legal[i][0];
+                index[1] = legal[i][1];
+                beta = min(score, beta);
+                if (beta <= alpha)
+                {
+                    break;
+                }
+            }
+        }
+    }
+    return best_score;
 }
+//g++ algorithm_A.cpp algorithm_TA.cpp player.cpp rules.cpp board.cpp chain_reaction.cpp gameTree.cpp
